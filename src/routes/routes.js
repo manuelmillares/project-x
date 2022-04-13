b980d3ac09081cea6cd6e7d97458fe9ffca54a7e
Manuel Millares <mm@atlas-reanalytics.com>
2022-04-13 14:52:41 +0200

    Add SignIn and SingUp component

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,10 +6,15 @@ import {
 
 import Invest from "../components/Invest/Invest";
 import Home from "../components/Home/Home";
+import SignIn from "../components/Auth/SignIn";
+import SignUp from "../components/Auth/SignUp";
+
 
 export const MyRoutes = () => {
     return <Routes>
       <Route path="/" element={<Home />} />
       <Route path="invest" element={<Invest />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="signup" element={<SignUp />} />
     </Routes>
 };
