f0c81ba0211d824bc78f711365222ac1fc50a1b0
Manuel Millares <mm@atlas-reanalytics.com>
2022-04-13 20:46:05 +0200

    Change HashRouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import ResponsiveAppBar from './components/navBar/navBar'
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { MyRoutes } from "./routes/routes";
 import { Container} from "@material-ui/core";
 
 
 function App() {
   return (
-    <Router basename={"/"}>
+    <Router basename={process.env.PUBLIC_URL}>
         <ResponsiveAppBar />
         <Container style={{paddingTop: "20%"}}>
           <MyRoutes />
