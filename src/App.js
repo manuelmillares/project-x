3671e96e5c4dbb0c18936ca816555dab8e9d4c04
Manuel Millares <mm@atlas-reanalytics.com>
2022-04-13 14:47:14 +0200

    Change app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/navBar/navBar'
+import { BrowserRouter } from "react-router-dom";
+import { MyRoutes } from "./routes/routes";
+import { Container} from "@material-ui/core";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={"/"}>
+        <ResponsiveAppBar />
+        <Container style={{paddingTop: "20%"}}>
+          <MyRoutes />
+        </Container>
+      </BrowserRouter>
   );
 }
 
