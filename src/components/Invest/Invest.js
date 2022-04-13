5bb0f35c68f2ddc407c296e3f72e56d241e42a90
Manuel Millares <mm@atlas-reanalytics.com>
2022-04-13 14:44:23 +0200

    Add invest component

---
+++ src/components/Invest/Invest.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import ProjectCard from '../Projects/ProjectCard';
+
+const Invest = () => {
+    return (
+        <div>
+        <div>Invest</div>
+        <ProjectCard />
+        </div>
+    )
+}
+
+export default Invest;
